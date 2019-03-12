ffc01294faf56f3162e91dcf4edbb121c6155ffa
Fulvio Corno <fulvio.corno@polito.it>
2019-03-12 15:04:47 +0100

    start implementing Libretto

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,5 +1,19 @@
 package it.polito.tdp.libretto;
 
-public class Libretto {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Libretto {
+	
+	private List<Voto> voti ;
+	
+	public Libretto() {
+		this.voti = new ArrayList<Voto>() ;
+	}
+		
+	public void add(Voto v) {
+		
+	}
+
+	
 }
